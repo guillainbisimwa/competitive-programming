42e114885166a8ff08b138e49baa9fbf104480ca
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-02-10 14:07:23 +0100

    Add swap case solution

---
+++ hackerrank/swap.py
@@ -0,0 +1,13 @@
+# sWAP cASE
+
+# Www.HackerRank.com → wWW.hACKERrANK.COM
+# Pythonist 2 → pYTHONIST 2  
+
+def swap_case(s):
+    return s.swapcase()
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
