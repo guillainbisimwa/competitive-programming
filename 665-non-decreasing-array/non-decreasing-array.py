2a775888abedc1eea8cd9a62e046689d7837f33a
Guillain bisimwa <guillainbisimwa@hotmail.fr>
2024-02-11 10:20:41 +0100

    Time: 127 ms (94.12%) | Memory: 13 MB (79.83%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        cnt = 0
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:
+                cnt += 1
+                if cnt > 1:
+                    return False
+                if i == 1 or nums[i - 2] <= nums[i]:
+                    nums[i - 1] = nums[i]
+                else:
+                    nums[i] = nums[i - 1]
+        return True
+        
