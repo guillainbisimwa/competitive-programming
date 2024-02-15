61558472882139f55feb93cdf92e573e83fc07a6
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-02-15 13:40:17 +0100

    Add Day_8_Dictionaries_and_Maps solution

---
+++ Day_8_Dictionaries_and_Maps.py
@@ -0,0 +1,20 @@
+# https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input().strip())
+
+annuaire = {}
+for _ in range(n):
+    nom, numero = input().split()
+    annuaire[nom] = numero
+
+while True:
+    try:
+        recherche = input().strip()
+        if recherche in annuaire:
+            print(f"{recherche}={annuaire[recherche]}")
+        else:
+            print("Not found")
+    except EOFError:
+        break
