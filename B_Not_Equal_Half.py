d00e7aa17948a25687b910c345557efe3e3408d5
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-03-04 09:52:50 +0100

    Add B not equal half solution

--- B_Not_Equal_Half.py
+++ B_Not_Equal_Half.py
@@ -1,21 +1,31 @@
 def rearrange_array(n, arr):
-    # Calculate the sum of the first and second halves of the array
+
+    l = len(arr)/2 
     sum_first_half = sum(arr[:n])
     sum_second_half = sum(arr[n:])
-    
-    # Check if the sums are equal
+
+    for i in range((n*2) - 1):
+        # print(arr)
+        # print()
+        sum_first_half = sum(arr[:n])
+        sum_second_half = sum(arr[n:])
+
+        if sum_first_half != sum_second_half:
+            return " ".join(map(str, arr))
+
+        # swap
+        for i in range((n*2) - 1):
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
     if sum_first_half == sum_second_half:
-        return "-1"  # No solution
-        
-    # Sort the array to rearrange it
-    arr.sort()
-    
-    # Output the rearranged array
-    return " ".join(map(str, arr))
+        return "-1" 
+
 
 # Read input values
 n = int(input())
 arr = list(map(int, input().split()))
 
-# Call the rearrange_array function and print the result
 print(rearrange_array(n, arr))
+
+# print(rearrange_array(3, [1,2,2,1,3,1]))
+# TODO : FAILD ON TEST 6 
