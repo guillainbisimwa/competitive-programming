cb7bab9f4aa19582b653a8c1d9c30b40a5d69ec0
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-03-12 07:34:33 +0100

    Add Division solution

---
+++ Python_Division.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    int_division = a // b
+
+    # Perform float division
+    float_division = a / b
+
+    # Print the results on separate lines
+    print(int_division)
+    print(float_division)
