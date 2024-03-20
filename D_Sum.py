9ab55d5858c93527d8d43038b3a5868c7a1b7f1b
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-03-20 10:24:30 +0100

    Add sum solution

--- D_Sum.py
+++ D_Sum.py
@@ -1,17 +1,25 @@
+def calculate_maximum_sum(size, k, arr):
+    maximum = 0
+    preSum = [0] * (size + 1)
 
-# solution to problem D
-t = int(input())
-for _ in range(t):
-    n, k = map(int, input().split())
-    arr = list(map(int, input().split()))
-    arr.sort()
-    pr_sum = [0] * (n + 1)
-    
-    for i in range(n):
-        pr_sum[i + 1] = pr_sum[i] + arr[i]
-    max_sum = 0
-    
+    # Calculate prefix sum
+    for i in range(size):
+        preSum[i + 1] = preSum[i] + arr[i]
+
+    # Iterate over possible values of i
     for i in range(k + 1):
-        current_max_sum = pr_sum[n - i] - pr_sum[2 * (k - i)]
-        max_sum = max(max_sum, current_max_sum)
-    print(max_sum)
+        maximum = max(maximum, (preSum[size - (k - i)] - preSum[i * 2]))
+
+    return maximum
+
+def main():
+    test = int(input())
+    for _ in range(test):
+        size, k = map(int, input().split())
+        arr = sorted(list(map(int, input().split())))
+
+        maximum = calculate_maximum_sum(size, k, arr)
+        print(maximum)
+
+if __name__ == "__main__":
+    main()
