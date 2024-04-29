e09e9299ef28b0bff8adc5a292be533605a2d537
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-04-29 22:07:56 +0100

    Add B  Mattia Binoton solution

--- B_Mattia_Binotto.py
+++ B_Mattia_Binotto.py
@@ -1,18 +1,12 @@
-def max_not_disappointed_drivers(n, pit_stop_times):
-    pit_stop_times.sort()
-    max_drivers = 0
-    wait_time = 0
-    
-    for pit_stop_time in pit_stop_times:
-        if pit_stop_time > wait_time:
-            max_drivers += 1
-        wait_time += pit_stop_time
-    
-    return max_drivers
+n = int(input())
+arr = list(map(int, input().split()))
 
-n = int(input().strip())
-pit_stop_times = list(map(int, input().strip().split()))
+arr.sort()
 
-result = max_not_disappointed_drivers(n, pit_stop_times)
+wait = count = 0
 
-print(result+1)
+for i in range(n):
+    if wait < arr[i]:
+        wait += arr[i]
+        count +=1
+print(count)
