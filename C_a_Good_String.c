aee12f786c23f3166bb52c58dc83256ccd48b262
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-05-08 15:59:33 +0100

    Add Good string solution

---
+++ C_a_Good_String.c
@@ -0,0 +1,30 @@
+def need_to_change(c, st, sp, s):
+    cnt = 0
+    for i in range(st, sp + 1):
+        if s[i] != c:
+            cnt += 1
+    return cnt
+
+def cal(c, st, sp, s):
+    if st == sp:
+        return 0 if s[st] == c else 1
+
+    mid = (st + sp) // 2
+    return min(need_to_change(c, st, mid, s) + cal(chr(ord(c) + 1), mid + 1, sp, s),
+               need_to_change(c, mid + 1, sp, s) + cal(chr(ord(c) + 1), st, mid, s))
+
+def solve():
+    n = int(input())
+    s = input().strip()
+
+    st, sp = 0, n - 1
+
+    print(cal('a', st, sp, s))
+
+def main():
+    testcase = int(input())
+    for _ in range(testcase):
+        solve()
+
+if __name__ == '__main__':
+    main()
