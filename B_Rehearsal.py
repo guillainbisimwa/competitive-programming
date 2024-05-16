2540294b90878f37c84c1e6f02225c55bf640e9a
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-05-16 12:55:58 +0100

    insert into a node

--- B_Rehearsal.py
+++ B_Rehearsal.py
@@ -1,13 +1,17 @@
 # step1
 t = int(input())
+# array = [[1,8],[2,5],[1,2]]
+# array = [[1,1],[2,5],[1,3],[2,6],[1,8],[1,4]]
 array = []
 for _ in range(t):
     x, y = list(map(int, input().split()))
     array.append([y, x])
+
 array.sort()
 #step2
 min_amount = 0
 left, right = 0, len(array)-1
+# print(array)
 while left <= right:
     min_amount = max(min_amount, array[left][0] + array[right][0])
     
@@ -21,3 +25,10 @@ while left <= right:
     if array[right][1] <= 0: right -= 1
     
 print(min_amount)
+
+
+
+
+
+# t = int(input())
+# for _ in range()
--- exercise/trees.py
+++ exercise/trees.py
@@ -1,9 +1,37 @@
 class Node:
-    def __init__(self, value) -> None:
-        self.data = value
-        self.left = None
+    def __init__(self, data) -> None:
+        self.data = data
         self.right = None
+        self.left = None
+
+class BST:
+    def __init__(self, root) -> None:
+        self.root = Node(root)
+
+    def insertElement(self, node, element) -> None:
+        if node:
+
+            if node.data > element:
+                # left
+                if node.left != None:
+                    self.insertElement(node.left, element)
+                else:
+                    node.left = Node(element)
+            else:
+                if node.right != None:
+                    self.insertElement(node.right, element)
+                else:
+                    node.right = Node(element)
 
 
+tree = BST(5)
 
-        
+# print(tree.root.data) # 5
+
+tree.insertElement(tree.root, 9)
+tree.insertElement(tree.root, 4)
+
+
+print(tree.root.right.data) # 9
+print(tree.root.left.data) # 4
+
