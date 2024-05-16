d404727fbc3e1b0b7c6c97c4aa72bd8af35a37bf
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-05-16 14:12:23 +0100

    Add preorder method

--- exercise/trees.py
+++ exercise/trees.py
@@ -8,18 +8,33 @@ class BST:
     def __init__(self, root) -> None:
         self.root = Node(root)
 
-    def insertElement(self, node, element) -> None:
+    def insertElementBinary(self, node, element) -> None:
         if node:
 
             if node.data > element:
                 # left
                 if node.left != None:
-                    self.insertElement(node.left, element)
+                    self.insertElementBinary(node.left, element)
                 else:
                     node.left = Node(element)
             else:
                 if node.right != None:
-                    self.insertElement(node.right, element)
+                    self.insertElementBinary(node.right, element)
+                else:
+                    node.right = Node(element)
+
+    def insertElement(self, node, element, ind) -> None:
+        if node:
+
+            if ind == "L":
+                # left
+                if node.left != None:
+                    self.insertElement(node.left, element, "L")
+                else:
+                    node.left = Node(element)
+            else:
+                if node.right != None:
+                    self.insertElement(node.right, element, "R")
                 else:
                     node.right = Node(element)
     
@@ -36,28 +51,40 @@ class BST:
         else:
             return False
 
-    def print(self, node, foundValues):
+    def printPreorder(self, node, foundValues):
 
         if node:
             foundValues += (str(node.data)+" - ")
             if node.left:
-                foundValues = self.print(node.left, foundValues)
+                foundValues = self.printPreorder(node.left, foundValues)
             if node.right:
-                foundValues = self.print(node.right, foundValues)
+                foundValues = self.printPreorder(node.right, foundValues)
             return foundValues
 
-tree = BST(5)
+tree = BST(1)
 
 # print(tree.root.data) # 5
+print(tree.printPreorder(tree.root, ""))
 
-tree.insertElement(tree.root, 9)
-tree.insertElement(tree.root, 4)
-tree.insertElement(tree.root, 2)
+tree.insertElement(tree.root, 2,"L")
+tree.insertElement(tree.root, 3, "R")
+tree.insertElement(tree.root, 4,"L")
+tree.insertElement(tree.root.left, 5,"R")
+tree.insertElement(tree.root.right, 6,"L")
+tree.insertElement(tree.root, 7, "R")
 
 
-print(tree.root.right.data) # 9
-print(tree.root.left.data) # 4
+# print(tree.root.right.data) # 9
+# print(tree.root.left.data) # 4
 
 print(tree.search(tree.root, 4))
-print(tree.print(tree.root, ""))
+print(tree.printPreorder(tree.root, ""))
+
+# print(tree.root.left.data) # 2
+# print(tree.root.left.left.data) # 4
+# print(tree.root.left.right.data) # 5
+# print(tree.root.right.data) # 3
+# print(tree.root.right.left.data) # 6
+# print(tree.root.right.right.data) # 7
+
 
