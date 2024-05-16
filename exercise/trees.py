bf547d9d5528392c51421262cb00bab25005eaf2
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-05-16 13:19:07 +0100

    Add a preorder search

--- exercise/trees.py
+++ exercise/trees.py
@@ -22,6 +22,19 @@ class BST:
                     self.insertElement(node.right, element)
                 else:
                     node.right = Node(element)
+    
+    def search(self,node, value):
+        """ 
+        loop through the tree from the root
+        """
+        if node:
+            if node.data == value:
+                return True
+            else:
+                return self.search(node.left, value) or  self.search(node.right, value) 
+                
+        else:
+            return False
 
 
 tree = BST(5)
@@ -35,3 +48,4 @@ tree.insertElement(tree.root, 4)
 print(tree.root.right.data) # 9
 print(tree.root.left.data) # 4
 
+print(tree.search(tree.root, 4))
