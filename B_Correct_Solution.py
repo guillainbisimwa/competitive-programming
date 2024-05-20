2419c654f06bc7b864876c13aa54725cce22ba4f
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-05-20 20:42:37 +0100

    Fix issues

--- B_Correct_Solution.py
+++ B_Correct_Solution.py
@@ -1,31 +1,21 @@
-# def bobAndAlice(bob, alice):
-#     """
-#     -------------input------------
-#     bob = 3310
-#     alice = 1033
-#     """
-# bob = int(input())
-# alice = int(input())
-# print(bobAndAlice(bob, alice))
-number = list(input())
-boob_answer = list(input())
+number = sorted(list(input()))
+bobs_answer = list(input())
 
 zeros = []
-smallest = ""
 answer = []
 i = 0
 
-while i < len(number) and number == "0":
-    answer.append(number[i])
-    i += 1
+while i < len(number) and number[i] == "0":
+    zeros.append(number[i])
+    i+=1
+    
 if i < len(number):
     answer.append(number[i])
-
-i += 1
+i+=1
 answer.extend(zeros)
 
 while i < len(number):
     answer.append(number[i])
-    i += 1
-print("OK" if answer == boob_answer else "WRONG_ANSWER")
-
+    i+=1
+    
+print("OK" if answer == bobs_answer else "WRONG_ANSWER")
