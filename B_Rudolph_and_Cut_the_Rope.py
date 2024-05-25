514521fb206898e5b9d4ba412bf0cf9ac8ddc44f
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-05-25 21:42:52 +0100

    Add rudolf and cut the rope solution

---
+++ B_Rudolph_and_Cut_the_Rope.py
@@ -0,0 +1,12 @@
+n = int(input())
+ 
+for _ in range(n):
+    v = int(input())
+    n = 0
+    for _ in range(v):
+        current  = input().split()
+
+        if(int(current [1]) < int(current [0])):
+            n += 1
+    
+    print(n)
