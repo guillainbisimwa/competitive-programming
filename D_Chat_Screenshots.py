5631a5b80346351c928ebfaa2de2667df9419418
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-08-06 22:49:02 +0200

    Fix issues

--- D_Chat_Screenshots.py
+++ D_Chat_Screenshots.py
@@ -12,7 +12,7 @@ def solve(data):
         k = int(data[index + 1])
         index += 2
         
-        G = [[] for _ in range(n + 1)]
+        graph = [[] for _ in range(n + 1)]
         a = [0] * (n + 1)
         d = [0] * (n + 1)
         
@@ -21,7 +21,7 @@ def solve(data):
                 a[i] = int(data[index])
                 index += 1
                 if i >= 3:
-                    G[a[i - 1]].append(a[i])
+                    graph[a[i - 1]].append(a[i])
                     d[a[i]] += 1
         
         q = deque()
@@ -33,7 +33,7 @@ def solve(data):
         while q:
             ans += 1
             u = q.popleft()
-            for v in G[u]:
+            for v in graph[u]:
                 d[v] -= 1
                 if d[v] == 0:
                     q.append(v)
