3960fbe1e80c49f5e413b33526a0cab4b01cf672
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-08-21 17:36:26 +0200

    Fix issues: make it divide by 25 sol

--- B_Make_it_Divisible_by_25.py
+++ B_Make_it_Divisible_by_25.py
@@ -1,31 +1,35 @@
-def search(v, s):
-    i, j = -1, -1
-    j = s.rfind(v[1])
-    if j != -1:
-        i = s.rfind(v[0], 0, j - 1)
-    k = s.rfind(v)
-    return max(i, k)
-
+INF = 100
+ 
+def solve(s, t):
+    sptr = len(s) - 1
+    ans = 0
+ 
+    while sptr >= 0 and s[sptr] != t[1]:
+        sptr -= 1
+        ans += 1
+ 
+    if sptr < 0:
+        return INF
+ 
+    sptr -= 1
+ 
+    while sptr >= 0 and s[sptr] != t[0]:
+        sptr -= 1
+        ans += 1
+ 
+    return INF if sptr < 0 else ans
+ 
 def main():
-    val = ["00", "25", "50", "75"]
     t = int(input())
-    
-    while t > 0:
-        s = input()
-        max_val = float('-inf')
-        
-        if int(s) % 25 == 0:
-            print("0")
-            t -= 1
-            continue
-
-        for v in val:
-            i = search(v, s)
-            if i > max_val:
-                max_val = i
-        
-        print(len(s) - max_val - 2)
-        t -= 1
-
+ 
+    for _ in range(t):
+        n = input()
+        ans = INF
+ 
+        for e in ["00", "25", "50", "75"]:
+            ans = min(ans, solve(n, e))
+ 
+        print(ans)
+ 
 if __name__ == "__main__":
     main()
