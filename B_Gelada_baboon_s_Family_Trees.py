7f0c779ce41cfd5114e9888a2ad8c9077547f843
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-11-05 22:17:51 +0200

    Add Gelada baboon solution

---
+++ B_Gelada_baboon_s_Family_Trees.py
@@ -0,0 +1,35 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # Path compression
+        return self.parent[x]
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        
+        if rootX != rootY:
+            # Union by rank
+            if self.rank[rootX] > self.rank[rootY]:
+                self.parent[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.parent[rootX] = rootY
+            else:
+                self.parent[rootY] = rootX
+                self.rank[rootX] += 1
+
+n = int(input().strip())
+p = list(map(int, input().strip().split()))
+
+uf = UnionFind(n)
+
+for i in range(n):
+    uf.union(i, p[i] - 1)
+
+unique_trees = len(set(uf.find(i) for i in range(n)))
+
+print(unique_trees)
